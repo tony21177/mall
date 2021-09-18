37f4874e0755ceaf0b9c26a8f7586f7659aa343f
Kun Nan <38462595+tony21177@users.noreply.github.com>
2021-09-18 15:45:28 +0800

    Update MallAdminApplication.java

--- mall-admin/src/main/java/com/macro/mall/MallAdminApplication.java
+++ mall-admin/src/main/java/com/macro/mall/MallAdminApplication.java
@@ -2,6 +2,7 @@ package com.macro.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 /**
  * 应用启动入口
